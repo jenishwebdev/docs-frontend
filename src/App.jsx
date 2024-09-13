52dcc1f567960efc711524bb6af2ad046354e35e
Jenish tailor <111968544+jenishwebdev@users.noreply.github.com>
2024-09-13 19:19:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
 // src/ChatApp.jsx
 
-import React, { useState, useEffect, useRef } from 'react';
-import io from 'socket.io-client';
+import React from 'react';
 import Doc from './page/doc';
-
-const SOCKET_SERVER_URL = 'http://localhost:3000'; // Replace with your server URL
-const socket = io(SOCKET_SERVER_URL, {
-  transports: ['websocket'], // Ensures WebSocket transport
-});
 
 const App = () => {
   return <Doc />
